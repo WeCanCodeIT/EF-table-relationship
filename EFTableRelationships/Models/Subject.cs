fbb03d72ae82469110089b142dd64145f184126b
Jen E <jenevans3ten@gmail.com>
2020-02-19 18:23:27 -0500

    removed remaining navigation properties

--- EFTableRelationships/Models/Subject.cs
+++ EFTableRelationships/Models/Subject.cs
@@ -10,8 +10,5 @@ namespace EFTableRelationships.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public Teacher Teacher { get; set; }
-
-        public List<StudentSubject> StudentSubjects { get; set; }
     }
 }
